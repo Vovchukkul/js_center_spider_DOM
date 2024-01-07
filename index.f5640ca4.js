94772f33f2aac337db5819a6d2c3fb23209b3ac7
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-07 08:07:29 +0200

    make build

---
+++ index.f5640ca4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),i=e.getBoundingClientRect(),l=t.getBoundingClientRect(),o=(i.height-l.height)/2,n=(i.width-l.width)/2;t.style.position="absolute",t.style.top=o+"px",t.style.left=n+"px";
+//# sourceMappingURL=index.f5640ca4.js.map
